ea0d6124749d50e8e55bbb52833acb9efa6c7896
Yuki-Shibanuma <smatosan@gmail.com>
2020-02-10 14:31:03 +0900

    init

---
+++ src/main/java/me/mckd/life/Life.java
@@ -0,0 +1,17 @@
+package me.mckd.life;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Life extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
