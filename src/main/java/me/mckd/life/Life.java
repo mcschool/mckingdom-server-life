cb3ea9295ac0413332aab8debeda7a893b92c6be
BuildTools <unconfigured@null.spigotmc.org>
2020-03-23 18:15:02 +0900

    life

--- src/main/java/me/mckd/life/Life.java
+++ src/main/java/me/mckd/life/Life.java
@@ -1,11 +1,13 @@
 package me.mckd.life;
 
+import me.mckd.life.World.lobby;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Life extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        new lobby(this);
         // Plugin startup logic
 
     }
---
+++ src/main/java/me/mckd/life/World/lobby.java
@@ -0,0 +1,42 @@
+package me.mckd.life.World;
+
+import me.mckd.life.Life;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.potion.PotionEffectType;
+
+public class lobby implements Listener {
+    public String worldName = "lobby";
+
+
+    public lobby(Life plugin) {
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+    }
+
+
+    public void PlayerJoinEvent(PlayerJoinEvent event){
+        Player player = event.getPlayer();
+        this.changeWorld(event.getPlayer());
+
+    }
+
+    public void onPlayerChangeWorld(PlayerChangedWorldEvent event) {
+        Player player = event.getPlayer();
+        if (player.getWorld().getName().equals(this.worldName)) {
+            this.changeWorld(player);
+            player.setGameMode(GameMode.SURVIVAL);
+        }
+    }
+
+    public void changeWorld(Player player) {
+        player.performCommand("mvtp world");
+        Location location = new Location(Bukkit.getWorld("world"), -106.5, 3, -1506.5);
+        player.teleport(location);
+    }
+}
+
