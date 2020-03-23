56a56bcee4bdd0a8e8451753449d4cd1e6903116
BuildTools <unconfigured@null.spigotmc.org>
2020-03-23 18:25:26 +0900

    life2

--- src/main/java/me/mckd/life/Life.java
+++ src/main/java/me/mckd/life/Life.java
@@ -7,12 +7,9 @@ public final class Life extends JavaPlugin {
 
     @Override
     public void onEnable() {
-<<<<<<< HEAD
         new lobby(this);
         // Plugin startup logic
-=======
         // Plugin startup logic..s
->>>>>>> f789d17c1de1001ba7db662b861bce0c35c8cdea
 
     }
 
