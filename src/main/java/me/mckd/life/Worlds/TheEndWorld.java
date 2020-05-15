d6f12014c4c2e2fe778146ba04aa9def60b1403a
Yuki-Shibanuma <smatosan@gmail.com>
2020-05-15 11:05:35 +0900

    a

--- src/main/java/me/mckd/life/Worlds/TheEndWorld.java
+++ src/main/java/me/mckd/life/Worlds/TheEndWorld.java
@@ -5,7 +5,7 @@ import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerGameModeChangeEvent;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
 
 public class TheEndWorld implements Listener {
 
@@ -18,14 +18,13 @@ public class TheEndWorld implements Listener {
         this.plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
 
-    // サバイバルワールドは管理者でもゲームモードの変更はできないように
     @EventHandler
-    public void onPlayerGameModeChangeEvent(PlayerGameModeChangeEvent e) {
-        if (e.getPlayer().getWorld().getName().equals(this.worldName)) {
-            Player player = e.getPlayer();
-            player.setGameMode(GameMode.SURVIVAL);
-            player.sendMessage("ゲームモードの変更はできません");
+    public void onPlayerChangeWorld(PlayerChangedWorldEvent e) {
+        Player player = e.getPlayer();
+        if (!player.getWorld().getName().equals(this.worldName)) {
+            return;
         }
+        player.setGameMode(GameMode.SURVIVAL);
     }
 
 }
