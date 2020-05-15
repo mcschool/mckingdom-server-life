2fc2c7a9fac67e129fa525285ab89d4c1e16cfd6
Yuki-Shibanuma <smatosan@gmail.com>
2020-05-15 10:17:10 +0900

    suv

--- src/main/java/me/mckd/life/Worlds/EndlessWorld.java
+++ src/main/java/me/mckd/life/Worlds/EndlessWorld.java
@@ -5,7 +5,9 @@ import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerGameModeChangeEvent;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 
 public class EndlessWorld implements Listener {
 
@@ -18,6 +20,16 @@ public class EndlessWorld implements Listener {
         this.plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
 
+    @EventHandler
+    public void onPlayerChangeWorld(PlayerChangedWorldEvent e) {
+        Player player = e.getPlayer();
+        if (!player.getWorld().getName().equals(this.worldName)) {
+            return;
+        }
+        player.setGameMode(GameMode.SURVIVAL);
+    }
+
+
     // サバイバルワールドは管理者でもゲームモードの変更はできないように
     @EventHandler
     public void onPlayerGameModeChangeEvent(PlayerGameModeChangeEvent e) {
@@ -27,4 +39,17 @@ public class EndlessWorld implements Listener {
             player.sendMessage("ゲームモードの変更はできません");
         }
     }
+
+    @EventHandler
+    public void onPlayerInteractEntity(PlayerInteractAtEntityEvent e) {
+        Player player = e.getPlayer();
+        if (!player.getWorld().getName().equals(this.worldName)) {
+            return;
+        }
+        String name = e.getRightClicked().getName();
+        if (name.equals("goto Lobby")) {
+            player.performCommand("mvtp lobby");
+        }
+    }
+
 }
