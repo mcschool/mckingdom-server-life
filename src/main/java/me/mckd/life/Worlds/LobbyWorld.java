3729b267a8e300179bfeca49c48924e80adf9e6b
Yuki-Shibanuma <smatosan@gmail.com>
2020-05-15 11:30:52 +0900

    a

--- src/main/java/me/mckd/life/Worlds/LobbyWorld.java
+++ src/main/java/me/mckd/life/Worlds/LobbyWorld.java
@@ -1,20 +1,20 @@
 package me.mckd.life.Worlds;
 
 import me.mckd.life.Life;
-import org.bukkit.Bukkit;
-import org.bukkit.GameMode;
-import org.bukkit.Location;
+import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 public class LobbyWorld implements Listener {
     public String worldName = "lobby";
@@ -107,11 +107,47 @@ public class LobbyWorld implements Listener {
         }
     }
 
+    /**
+     * 換金するインベントリを開く
+     * @param player
+     */
     public void openCashOffice(Player player) {
         Inventory inv;
         inv = Bukkit.createInventory(null, 9, "換金所");
         inv.clear();
         player.openInventory(inv);
+    }
+
+    @EventHandler
+    public void onInventoryCloseEvent(InventoryCloseEvent e) {
+        World world = e.getPlayer().getWorld();
+        if (!world.getName().equals(this.worldName)) {
+            return;
+        }
+        Player player = (Player) e.getPlayer();
+        Inventory inv = e.getInventory();
+        String invName = inv.getName();
+        if (invName.equals("換金所")) {
+            this.changeMoney(player, inv);
+        }
+    }
+
+    public void changeMoney(Player player, Inventory inv) {
+        int price = 0;
+        for (int i = 0; i < inv.getSize(); i++) {
+            ItemStack item = inv.getItem(i);
+            Bukkit.getLogger().info(item.toString());
+            if (item.getType() != null) {
+                price += this.getItemPrice(item);
+            }
+        }
+        player.sendMessage(price + "円になりました");
+    }
+
+    public int getItemPrice(ItemStack item) {
+        Material type = item.getType();
+        if (type == Material.DIAMOND) return 1000;
+        return 1;
     }
 }
 
