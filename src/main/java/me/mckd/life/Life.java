325d56d2c2b12e7a8e99664d0261e75f97c3976b
Yuki-Shibanuma <smatosan@gmail.com>
2020-05-25 13:16:51 +0900

    fix conflict

--- src/main/java/me/mckd/life/Life.java
+++ src/main/java/me/mckd/life/Life.java
@@ -2,11 +2,8 @@ package me.mckd.life;
 
 import me.mckd.life.Worlds.LobbyWorld;
 import me.mckd.life.Worlds.EndlessWorld;
-<<<<<<< HEAD
 import me.mckd.life.Worlds.Monster;
-=======
 import me.mckd.life.Worlds.NetherWorld;
->>>>>>> c799ffd8c76fc5e1bf5ba0399930173b214c99e6
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Life extends JavaPlugin {
@@ -19,11 +16,8 @@ public final class Life extends JavaPlugin {
         // 各種コードをロード
         new LobbyWorld(this);
         new EndlessWorld(this);
-<<<<<<< HEAD
         new Monster(this);
-=======
         new NetherWorld(this);
->>>>>>> c799ffd8c76fc5e1bf5ba0399930173b214c99e6
     }
 
     @Override
