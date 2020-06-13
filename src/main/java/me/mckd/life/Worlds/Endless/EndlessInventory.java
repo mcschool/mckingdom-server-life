d62c844002122ab00e16c070db033254be1395fc
BuildTools <unconfigured@null.spigotmc.org>
2020-06-13 15:12:25 +0900

    test9

--- src/main/java/me/mckd/life/Worlds/Endless/EndlessInventory.java
+++ src/main/java/me/mckd/life/Worlds/Endless/EndlessInventory.java
@@ -5,7 +5,6 @@ import org.bukkit.Material;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,7 +19,7 @@ public class EndlessInventory {
         ItemMeta lifeMate = life.getItemMeta();
         lifeMate.setDisplayName("自分の家");
         List<String> lifeLores = new ArrayList<>();
-        lifeLores.add("life");
+        lifeLores.add("これをクリックで自分の家にテレポートできます!もし自分の家を設定してないなら運営に言ってください!");
         lifeMate.setLore(lifeLores);
         life.setItemMeta(lifeMate);
         inv.setItem(5, life);
