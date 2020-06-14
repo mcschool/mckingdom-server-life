683e6479de82e5768d771a04f32bfd7d467b550a
BuildTools <unconfigured@null.spigotmc.org>
2020-06-14 09:05:01 +0900

    test

--- src/main/java/me/mckd/life/Worlds/Endless/EndlessWorld.java
+++ src/main/java/me/mckd/life/Worlds/Endless/EndlessWorld.java
@@ -223,7 +223,7 @@ public class EndlessWorld implements Listener {
                 Location location = new Location(player.getWorld(), 87, 69, 325);
                 player.teleport(location);
             }
-            e.setCancelled(true);
+            //e.setCancelled(true);
         }
 
         return;
